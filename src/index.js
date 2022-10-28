090cf9d7a141d2f9db2fa8943f5b0a509f4f9e64
unknown <nguyenvanlinh7@outlook.com>
2022-10-28 23:09:13 +0700

    feature js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-document.getElementById("");
+var menu =  document.getElementById("menu-nav");
+var activemenu = document.querySelector('.active-menu')
+
+menu.onclick = function() {
+    activemenu.classList.toggle('open')
+}
