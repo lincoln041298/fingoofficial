e82d2a13dc385114174ccb0495d8bc05df6f6d8e
unknown <nguyenvanlinh7@outlook.com>
2022-10-31 21:29:45 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // var menu = document.querySelector(".menu-opennav");
-var menu = document.getElementById('menu-opennav')
+var menu = document.getElementById("menu-opennav");
 var activemenu = document.querySelector(".active-menu");
 var closeButton = document.getElementById("close-button");
-var listMenu = document.getElementById("list-menu")
+var listMenu = document.getElementById("list-menu");
 
 menu.onclick = function () {
   activemenu.classList.toggle("open");
@@ -12,9 +12,24 @@ closeButton.onclick = function () {
   activemenu.classList.remove("open");
 };
 
-document.onclick = function(e) {
-  
-    if(e.target.id != "menu-opennav" && e.target.id != "list-menu") {
-        activemenu.classList.remove("open");
-    }
+document.onclick = function (e) {
+  if (e.target.id != "menu-opennav" && e.target.id != "list-menu") {
+    activemenu.classList.remove("open");
+  }
+};
+
+function openTag(e, tagName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+
+  document.getElementById(tagName).style = "display:grid";
+  e.currentTarget.className += " active";
 }
